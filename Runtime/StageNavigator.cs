f8bebfdc679da57065f3eb0b8c4bd9fe2434a4f5
sus-taguchi-t <taguchi-t@m.sus-g.co.jp>
2022-11-30 16:02:58 +0900

    Remove transition history function

--- Runtime/StageNavigator.cs
+++ Runtime/StageNavigator.cs
@@ -30,10 +30,7 @@ namespace Extreal.Core.StageNavigation
         public event Action<TStage> OnStageTransitioned;
 
         private readonly Dictionary<TStage, TScene[]> stageMap = new Dictionary<TStage, TScene[]>();
-        private readonly Stack<TStage> stageHistory = new Stack<TStage>();
         private readonly List<TScene> loadedScenes = new List<TScene>();
-        private bool initialTransition = true;
-        private TStage currentStage;
 
         /// <summary>
         /// Creates a new StageNavigator with given configuration
@@ -68,65 +65,16 @@ namespace Extreal.Core.StageNavigation
         /// </summary>
         /// <param name="stage">Stage Name to transition to</param>
         /// <returns>UniTask of this method</returns>
-        public async UniTask ReplaceAsync(TStage stage)
+        public async UniTask TransitionAsync(TStage stage)
         {
+            Logger.LogDebug($"Transitions to '{stage}'");
+
             OnStageTransitioning?.Invoke(stage);
-            if (initialTransition)
-            {
-                initialTransition = false;
-            }
 
             await UnloadScenesAsync(stage);
             await LoadScenesAsync(stage);
 
-            currentStage = stage;
-            Debug(Operation.Replace, currentStage);
-            OnStageTransitioned?.Invoke(currentStage);
-        }
-
-        /// <inheritdoc/>
-        public async UniTask PushAsync(TStage stage)
-        {
-            OnStageTransitioning?.Invoke(stage);
-            if (!initialTransition)
-            {
-                stageHistory.Push(currentStage);
-            }
-            else
-            {
-                initialTransition = false;
-            }
-
-            await UnloadScenesAsync(stage);
-            await LoadScenesAsync(stage);
-
-            currentStage = stage;
-            Debug(Operation.Push, currentStage);
-            OnStageTransitioned?.Invoke(currentStage);
-        }
-
-        /// <inheritdoc/>
-        public async UniTask PopAsync()
-        {
-            if (stageHistory.Count == 0)
-            {
-                throw new InvalidOperationException("there is no stage transition history");
-            }
-
-            currentStage = stageHistory.Pop();
-            OnStageTransitioning?.Invoke(currentStage);
-            await UnloadScenesAsync(currentStage);
-            await LoadScenesAsync(currentStage);
-
-            Debug(Operation.Pop, currentStage);
-            OnStageTransitioned?.Invoke(currentStage);
-        }
-
-        /// <inheritdoc/>
-        public void Reset()
-        {
-            stageHistory.Clear();
-            Debug(Operation.Reset);
+            OnStageTransitioned?.Invoke(stage);
         }
 
         private async UniTask UnloadScenesAsync(TStage stage)
@@ -180,30 +128,6 @@ namespace Extreal.Core.StageNavigation
                 }
             }
             return isDone;
-        }
-
-        private static void Debug(Operation operation, TStage stage)
-        {
-            if (Logger.IsDebug())
-            {
-                Logger.LogDebug($"{operation}: {stage}");
-            }
-        }
-
-        private static void Debug(Operation operation)
-        {
-            if (Logger.IsDebug())
-            {
-                Logger.LogDebug(operation.ToString());
-            }
-        }
-
-        private enum Operation
-        {
-            Replace,
-            Push,
-            Pop,
-            Reset,
         }
     }
 }
